e36af8750d20d9b90cf6263d013d566e63f41f9e
minhtripr3 <phamminhtripr3@gmail.com>
2025-08-10 16:41:58 +0700

    video19

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -7,8 +7,22 @@ class MyComponent extends React.Component {
         address: "Khánh Hòa - Việt Nam",
         age: 21
     };
+    handleClick(event) {
+        console.log("click me");
+        console.log(event);
+    }
+    handleMouseOver(event) {
+
+        console.log(event);
+    }
     render() {
-        return <div> tôi tên là {this.state.name} đến từ {this.state.address} tuổi {this.state.age}</div>;
+        return <div>
+            tôi tên là {this.state.name} đến từ {this.state.address} tuổi {this.state.age}
+            <button onClick={this.handleClick}>click vào đây</button>
+            <button onMouseOver={this.handleMouseOver}>hover me</button>
+        </div>;
+
+
     }
 }
 
