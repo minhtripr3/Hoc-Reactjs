bb57efa70b443c3df22e8d9adf8f045737fc843a
minhtripr3 <phamminhtripr3@gmail.com>
2025-08-10 15:24:33 +0700

    video 16

---
+++ src/component/MyComponent.js
@@ -0,0 +1,10 @@
+// src/components/MyComponent.js
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return <div>My component</div>;
+    }
+}
+
+export default MyComponent;
