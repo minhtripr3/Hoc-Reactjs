0309554006143aedab838ed302b25bd2e3037b7d
minhtripr3 <phamminhtripr3@gmail.com>
2025-08-14 20:41:48 +0700

    video25

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,7 +1,13 @@
 import React from "react";
 class DisplayInfo extends React.Component {
-
-
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
         console.log(this.props);
         // destructuring props
@@ -10,22 +16,28 @@ class DisplayInfo extends React.Component {
 
         return (
             <div>
-                {ListUser.map((user) => {
-                    return (
-                        <div key={user.id}>
-
-                            <div>Tên : {user.name} </div>
-                            <div>Tuổi : {user.age} </div>
-                            <hr></hr>
-
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser === true ? "Hide List User" : "Show list User"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {ListUser.map((user) => {
+                            return (
+                                <div key={user.id} className={user.age < 18 ? "red" : "green"}>
+                                    <div>Tên : {user.name} </div>
+                                    <div>Tuổi : {user.age} </div>
+                                    <hr></hr>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+                <div>
+                    <span>Danh sách người dùng</span>
+                </div>
             </div>
-            // <div>
-            //     <div>Tên : {name} </div>
-            //     <div>Tuổi : {age} </div>
-            // </div>
         )
     }
 }
