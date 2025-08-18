45abf919e6c6f30760b7c08f61c48a3735795a30
minhtripr3 <phamminhtripr3@gmail.com>
2025-08-18 20:56:53 +0700

    video hook funtion thay vì class component

--- src/component/AddUserInfo.js
+++ src/component/AddUserInfo.js
@@ -1,59 +1,103 @@
-import React from 'react';
+import React, { useState } from "react";
 import MyComponent from './MyComponent';
-class AddUserInfo extends React.Component {
-    state = {
-        name: "Pham Minh Trí",
-        address: "Khánh Hòa - Việt Nam",
-        age: 21
+// class AddUserInfo extends React.Component {
+//     state = {
+//         name: "Pham Minh Trí",
+//         address: "Khánh Hòa - Việt Nam",
+//         age: 21
+//     };
+
+
+
+//     handeleOnchangeInput = (event) => {
+//         this.setState({
+//             name: event.target.value
+//         })
+//         console.log(event.target.value);
+//     }
+//     handeleOnchangeAge = (event) => {
+//         this.setState({
+//             age: event.target.value
+//         })
+//         console.log(event.target.value);
+//     }
+
+//     handleOnSubmit = (event) => {
+//         event.preventDefault(); // Ngăn chặn hành động mặc định của form
+//         console.log(this.state);
+//         // Bạn có thể thực hiện các hành động khác sau khi submit form
+//         this.props.handleAddNewUser({
+//             id: Math.floor(Math.random() * 1000 + 1) + 'random',
+//             name: this.state.name,
+//             age: this.state.age
+//         });
+
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 tôi tên là {this.state.name} đến từ {this.state.address} tuổi {this.state.age}
+//                 <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+//                     <label>tên :</label>
+//                     <input
+//                         value={this.state.name}
+//                         type="text"
+//                         onChange={(event) => { this.handeleOnchangeInput(event) }} />
+//                     <label>tuổi :</label>
+//                     <input
+//                         value={this.state.age}
+//                         type="text"
+//                         onChange={(event) => { this.handeleOnchangeAge(event) }} />
+//                     <button> submit</button>
+//                 </form>
+
+//             </div>
+
+//         )
+//     }
+// }
+
+const AddUserInfo = (props) => {
+    console.log("check props", props);
+    const [name, setName] = useState("Pham Minh Trí");
+    const [address, setAddress] = useState("Khánh Hòa - Việt Nam");
+    const [age, setAge] = useState(21);
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault(); // Ngăn chặn hành động mặc định của form
+        props.handleAddNewUser({
+            id: Math.floor(Math.random() * 1000 + 1) + "random",
+            name: name,
+            age: age,
+        });
     };
 
-
-
-    handeleOnchangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-        console.log(event.target.value);
+    const handeleOnchangeInput = (event) => {
+        setName(event.target.value);
     }
-    handeleOnchangeAge = (event) => {
-        this.setState({
-            age: event.target.value
-        })
-        console.log(event.target.value);
+    const handeleOnchangeAge = (event) => {
+        setAge(event.target.value);
     }
+    return (
+        <div>
+            tôi tên là {name} đến từ {address} tuổi {age}
+            <form onSubmit={(event) => { handleOnSubmit(event) }}>
+                <label>tên :</label>
+                <input
+                    value={name}
+                    type="text"
+                    onChange={(event) => { handeleOnchangeInput(event) }} />
+                <label>tuổi :</label>
+                <input
+                    value={age}
+                    type="text"
+                    onChange={(event) => { handeleOnchangeAge(event) }} />
+                <button> submit</button>
+            </form>
 
-    handleOnSubmit = (event) => {
-        event.preventDefault(); // Ngăn chặn hành động mặc định của form
-        console.log(this.state);
-        // Bạn có thể thực hiện các hành động khác sau khi submit form
-        this.props.handleAddNewUser({
-            id: Math.floor(Math.random() * 1000 + 1) + 'random',
-            name: this.state.name,
-            age: this.state.age
-        });
+        </div>
 
-    }
-    render() {
-        return (
-            <div>
-                tôi tên là {this.state.name} đến từ {this.state.address} tuổi {this.state.age}
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <label>tên :</label>
-                    <input
-                        value={this.state.name}
-                        type="text"
-                        onChange={(event) => { this.handeleOnchangeInput(event) }} />
-                    <label>tuổi :</label>
-                    <input
-                        value={this.state.age}
-                        type="text"
-                        onChange={(event) => { this.handeleOnchangeAge(event) }} />
-                    <button> submit</button>
-                </form>
+    )
 
-            </div>
-
-        )
-    }
 }
 export default AddUserInfo;    
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,55 +1,102 @@
 // src/components/MyComponent.js
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfo from "./AddUserInfo";
 import DisplayInfo from "./DisplayInfo";
 
-class MyComponent extends React.Component {
-    state = {
-        ListUser: [
-            { id: 1, name: "Hoidankhanhhoa1", age: 19 },
-            { id: 2, name: "Hoidankhanhhoa2", age: 20 },
-            { id: 3, name: "Hoidankhanhhoa3", age: 21 }
+// class MyComponent extends React.Component {
+//     state = {
+//         ListUser: [
+//             { id: 1, name: "Hoidankhanhhoa1", age: 19 },
+//             { id: 2, name: "Hoidankhanhhoa2", age: 20 },
+//             { id: 3, name: "Hoidankhanhhoa3", age: 21 }
 
-        ]
+//         ]
 
+//     }
+//     handleAddNewUser = (userOject) => {
+//         console.log("check handleAddNewUser", userOject);
+//         this.setState({
+//             ListUser: [userOject, ...this.state.ListUser,]
+//         })
+//     }
+//     handelDeleteUser = (userId) => {
+//         let ListUserClone = this.state.ListUser;
+//         ListUserClone = ListUserClone.filter(item => item.id !== userId);
+//         this.setState({
+//             ListUser: ListUserClone
+//         })
+//     }
+//     render() {
+//         return (
+//             <>
+//                 <div className="a">
+//                     <AddUserInfo
+//                         handleAddNewUser={this.handleAddNewUser}
+//                     />
+//                     <br />
+//                     <DisplayInfo
+//                         ListUser={this.state.ListUser}
+//                         handelDeleteUser={this.handelDeleteUser}
+//                     ></DisplayInfo>
+
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+// const MyComponent = () => {
+//     const [ListUser, setListUser] = React.useState([
+//         { id: 1, name: "Hoidankhanhhoa1", age: 19 },
+//         { id: 2, name: "Hoidankhanhhoa2", age: 20 },
+//         { id: 3, name: "Hoidankhanhhoa3", age: 21 }
+//     ]);
+
+//     const handleAddNewUser = (userObject) => {
+//         setListUser([userObject, ...ListUser]);
+//     };
+
+//     const handelDeleteUser = (userId) => {
+//         setListUser(ListUser.filter(item => item.id !== userId));
+//     };
+
+//     return (
+//         <div className="a">
+//             <AddUserInfo handleAddNewUser={handleAddNewUser} />
+//             <br />
+//             <DisplayInfo ListUser={ListUser} handelDeleteUser={handelDeleteUser} />
+//         </div>
+//     );
+// };
+const MyComponent = (props) => {
+    const [ListUser, setListUser] = useState([
+        { id: 1, name: "Pham Minh Tri 1", age: 19 },
+        { id: 2, name: "Pham Minh Tri 2", age: 20 },
+        { id: 3, name: "Pham Minh Tri 3", age: 21 }
+    ])
+    const handleAddNewUser = (user) => {
+        setListUser([user, ...ListUser]);
     }
-    handleAddNewUser = (userOject) => {
-        console.log("check handleAddNewUser", userOject);
-        this.setState({
-            ListUser: [userOject, ...this.state.ListUser,]
-        })
+    const handelDeleteUser = (userId) => {
+        setListUser(ListUser.filter(item => item.id !== userId));
     }
-    handelDeleteUser = (userId) => {
-        let ListUserClone = this.state.ListUser;
-        ListUserClone = ListUserClone.filter(item => item.id !== userId);
-        this.setState({
-            ListUser: ListUserClone
-        })
-    }
-
-    render() {
-
-        return (
-            <>
-                <div className="a">
-                    <AddUserInfo
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <br />
-                    <DisplayInfo
-                        ListUser={this.state.ListUser}
-                        handelDeleteUser={this.handelDeleteUser}
-                    ></DisplayInfo>
-
-                </div>
-
-            </>
-        )
+    return (
+        <>
+            <div className="a">
+                <AddUserInfo handleAddNewUser={handleAddNewUser} />
 
 
+                <br />
+                <DisplayInfo
+                    ListUser={ListUser}
+                    handelDeleteUser={handelDeleteUser}
+                ></DisplayInfo>
 
+            </div>
+        </>
+    )
 
-    }
 }
+
+
 
 export default MyComponent;
